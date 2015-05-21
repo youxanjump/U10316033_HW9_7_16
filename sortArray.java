ee36795610f69046c66aabcc90531af398be6d7f
youxanjump <maydayisgod@hotmail.com.tw>
2015-05-21 17:54:07 +0800

    利用sort的方法將座標由小到大排列(第一階段排列)
    首先先判斷x座標
    利用泡泡排序法
    然後要交換的時候必須整個座標一起交換(5~16行)
    然後將其輸出
    原本直接輸出的結果是{{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}}
    經過第一階段排序之後變成{{1,7},{1,2},{1,1},{4,2},{4,5},{4,1}}

--- sortArray.java
+++ sortArray.java
@@ -1,25 +1,64 @@
 public class sortArray{
 
-  public static void main(String[] args){
+	public static void sort(int m[][]){
 	
-	  	int[][] m = {{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}};
-		
-  		for(int x = 0; x < 6; x++){
-  			for(int y = 0; y < 2; y++){
-	  			if(x == 0 && y == 0){
-	  				System.out.print("{{" + m[x][y] + ",");
-	  			}
-	  			else if(x == 5 && y == 1){
-		  			System.out.print(m[x][y] + "}}");
-		  		}
-		  		else if(x != 0 && y == 0){
-		  			System.out.print("{" + m[x][y] + ",");
-	  			}
-			  	else if(x != 5 && y == 1){
-			  		System.out.print(m[x][y] + "},");
-			  	}
+		for(int x = 0; x < 6; x++){
+			for(int i = 1; i <= 5-x; i++){
+				if(m[x+i][0] < m[x][0]){
+					int temp = m[x+i][0];
+					m[x+i][0] = m[x][0];
+					m[x][0] = temp;
+					int temp1 = m[x+i][1];
+					m[x+i][1] = m[x][1];
+					m[x][1] = temp1;
+				}
+			}
+		}
+	
+		for(int x = 0; x < 6; x++){
+			for(int y = 0; y < 2; y++){
+				if(x == 0 && y == 0){
+					System.out.print("{{" + m[x][y] + ",");
+				}
+				else if(x == 5 && y == 1){
+					System.out.print(m[x][y] + "}}");
+				}
+				else if(x != 0 && y == 0){
+					System.out.print("{" + m[x][y] + ",");
+				}
+				else if(x != 5 && y == 1)
+					System.out.print(m[x][y] + "},");
 				
-		  	}
-	  	}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){
+	
+		int[][] m = {{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}};
+		
+		for(int x = 0; x < 6; x++){
+			for(int y = 0; y < 2; y++){
+				if(x == 0 && y == 0){
+					System.out.print("{{" + m[x][y] + ",");
+				}
+				else if(x == 5 && y == 1){
+					System.out.print(m[x][y] + "}}");
+				}
+				else if(x != 0 && y == 0){
+					System.out.print("{" + m[x][y] + ",");
+				}
+				else if(x != 5 && y == 1)
+					System.out.print(m[x][y] + "},");
+				
+			}
+		}
+		
+		System.out.println();
+		
+		sort(m);
+	
+	
 	}
 }
